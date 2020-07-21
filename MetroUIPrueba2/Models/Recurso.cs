0314314612598aaec2f6f098752a4637c7b8f7b6
Car1986 <67611245+Car1986@users.noreply.github.com>
2020-07-20 21:26:29 -0400

    Agregacion de algunas cosas
    Agregacion de algunas cosas

--- MetroUIPrueba2/Models/Recurso.cs
+++ MetroUIPrueba2/Models/Recurso.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace MetroUIPrueba2.Models
 {
-    public class Insumo
+    public class Recurso
     {
         public int Id { get; set; }
         [Required]
