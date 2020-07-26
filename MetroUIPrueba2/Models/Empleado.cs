6bb1ba37c8f8598a5480fab1ee328a3323da62d5
Car1986 <67611245+Car1986@users.noreply.github.com>
2020-07-26 18:36:41 -0400

    cambios menores e base de datos
    cambios menores e base de datos

--- MetroUIPrueba2/Models/Empleado.cs
+++ MetroUIPrueba2/Models/Empleado.cs
@@ -10,14 +10,19 @@ namespace MetroUIPrueba2.Models
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(11, MinimumLength = 10)]
         public string Rut  { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage =" El o los nombres deben de tener un mínimo de 1 y máximo 50 caracteres")]
         public string Nombres { get; set; }
         [Required]
+        [StringLength(80, MinimumLength = 1, ErrorMessage = " El o los nombres deben de tener un mínimo de 1 y máximo 80 caracteres")]
         public string Apellidos { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = " El o los nombres deben de tener un mínimo de 1 y máximo 100 caracteres")]
         public string Direccion { get; set; }
         [Required]
+        [StringLength(16, MinimumLength = 9, ErrorMessage = " El telefono debe de tener un maximo de 16 números y un minimo de 9")]
         public string Telefono { get; set; }
         [Required]
         public string Correo { get; set; }
